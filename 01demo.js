757810c501c43cddd9a983cf1ade30e4f67982a5
YosefYuan <yuanruixiangjet@163.com>
2018-01-24 00:35:56 +0800

    0124

---
+++ 01demo.js
@@ -0,0 +1,11 @@
+const element = (
+    <h1 className="greeting">
+        Hello, world!
+    </h1>
+);
+
+const element = React.createElement(
+    'h1',
+    { className: 'greeting' },
+    'Hello world!'
+);
---
+++ demo.jsx
@@ -0,0 +1,37 @@
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+
+    tick() {
+        this.setState({
+            data: new Date()
+        });
+    }
+}
+
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
